860902096c62593535b77cbdc7ae68a8cb2e6080
IceReign-MEXT <MexRob@proton.me>
2025-08-05 05:51:12 +0100

    Update mex.py

--- mex.py
+++ mex.py
@@ -1,7 +1,11 @@
-PRIVATE_KEY=0xabc123yourprivatekey
-SAFE_ADDRESS=0xYourSafeWalletAddress
-WATCH_ADDRESS=0xCompromisedWalletAddress
-RPC_URL=https://eth.llamarpc.com
-TELEGRAM_BOT_TOKEN=12345678:ABCDEFyourbottoken
-TELEGRAM_CHAT_ID=123456789
+from dotenv import load_dotenv
+import os
 
+load_dotenv()  # Make sure this is at the top of your script
+
+PRIVATE_KEY = os.getenv("PRIVATE_KEY")
+SAFE_ADDRESS = os.getenv("SAFE_ADDRESS")
+WATCH_ADDRESS = os.getenv("WATCH_ADDRESS")
+RPC_URL = os.getenv("RPC_URL")
+TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
